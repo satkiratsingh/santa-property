8423e4ce1b476f07b8f5fdb5b2e5d735d1086ccf
Satkirat Singh <anmolssl@outlook.com>
2024-08-23 17:49:50 +0530

    Create property listing section on home page

---
+++ src/static/propertyListings.js
@@ -0,0 +1,30 @@
+import listing_1 from "../images/property-listings/1.png";
+import listing_2 from "../images/property-listings/2.png";
+import listing_3 from "../images/property-listings/3.png";
+
+export const propertyListings = [
+  {
+    image: listing_1,
+    price: "Price",
+    location: "Property Location",
+    bed: "3",
+    bath: "4",
+    land_size: "150 sq yards.",
+  },
+  {
+    image: listing_2,
+    price: "Price",
+    location: "Property Location",
+    bed: "3",
+    bath: "3",
+    land_size: "200 sq yards.",
+  },
+  {
+    image: listing_3,
+    price: "Price",
+    location: "Property Location",
+    bed: "5",
+    bath: "5",
+    land_size: "300 sq yards.",
+  },
+];

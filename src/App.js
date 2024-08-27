cdc8a1418b410dc53fec0ce387fd471e1a0716e3
Satkirat Singh <72094951+satkiratsingh@users.noreply.github.com>
2024-08-27 20:01:13 +0530

    Implmented Footer with responsiveness (#7)

--- src/App.js
+++ src/App.js
@@ -216,6 +216,23 @@ function App() {
           </div>
         </div>
       </section>
+
+      <footer className="bg-black h-[24vh] px-20 items-center flex flex-col justify-center w-full">
+        <div className="flex flex-row items-center justify-between w-full max-sm:gap-4 max-sm:flex-col-reverse">
+          <h1 className={`text-[20px] font-bold text-white`}>Santa Property</h1>
+          <div className="flex flex-row items-center gap-4 text-white">
+            <a className="hover:font-bold" href="#/">
+              Home
+            </a>
+            <a className="hover:font-bold" href="#/about">
+              About
+            </a>
+            <a className="hover:font-bold" href="#/contact">
+              Contact
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
